d64555915ac1abc0a37c5275716f9595e9a58e11
Pei Han <hanpeic@gmail.com>
2023-07-20 23:30:11 +1000

    init status of shop

--- app/models/disco_app/session_storage.rb
+++ app/models/disco_app/session_storage.rb
@@ -6,6 +6,7 @@ module DiscoApp
         shop = DiscoApp::Shop.find_or_initialize_by(shopify_domain: session.shop)
         shop.shopify_token = session.access_token
         shop.access_scopes = session.scope.to_s
+        shop.status = DiscoApp::Shop::statuses[:never_installed]
         shop.save!
         shop.id
       end
